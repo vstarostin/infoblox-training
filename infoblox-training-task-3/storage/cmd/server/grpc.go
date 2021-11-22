eb06015de62a12e9e2c0b5f3a5145410ce682403
Vyacheslav Starostin <starostin29.12@gmail.com>
2021-11-22 10:20:51 +0300

    Add Storage project layout

---
+++ infoblox-training-task-3/storage/cmd/server/grpc.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"infoblox-training-task-3/storage/pkg/dapr"
+	"infoblox-training-task-3/storage/pkg/pb"
+	"infoblox-training-task-3/storage/pkg/svc"
+	"time"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	grpc_validator "github.com/grpc-ecosystem/go-grpc-middleware/validator"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/infobloxopen/atlas-app-toolkit/gateway"
+	"github.com/infobloxopen/atlas-app-toolkit/requestid"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/reflection"
+)
+
+func NewGRPCServer(logger *logrus.Logger, pubsub *dapr.PubSub, dbConnectionString string) (*grpc.Server, error) {
+	grpcServer := grpc.NewServer(
+		grpc.KeepaliveParams(
+			keepalive.ServerParameters{
+				Time:    time.Duration(viper.GetInt("config.keepalive.time")) * time.Second,
+				Timeout: time.Duration(viper.GetInt("config.keepalive.timeout")) * time.Second,
+			},
+		),
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(
+				// logging middleware
+				grpc_logrus.UnaryServerInterceptor(logrus.NewEntry(logger)),
+
+				// Request-Id interceptor
+				requestid.UnaryServerInterceptor(),
+
+				// Metrics middleware
+				grpc_prometheus.UnaryServerInterceptor,
+
+				// validation middleware
+				grpc_validator.UnaryServerInterceptor(),
+
+				// collection operators middleware
+				gateway.UnaryServerInterceptor(),
+			),
+		),
+	)
+
+	// create new postgres database
+	db, err := gorm.Open("postgres", dbConnectionString)
+	if err != nil {
+		return nil, err
+	}
+	// register service implementation with the grpcServer
+	s, err := svc.NewBasicServer(pubsub, db)
+	if err != nil {
+		return nil, err
+	}
+	pb.RegisterStorageServer(grpcServer, s)
+	// Register reflection service on gRPC server.
+	reflection.Register(grpcServer)
+
+	return grpcServer, nil
+}
---
+++ infoblox-training-task-3/storage/cmd/server/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"infoblox-training-task-3/storage/pkg/dapr"
+	"log"
+	"net"
+	"strings"
+
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+
+	"github.com/infobloxopen/atlas-app-toolkit/server"
+
+	"github.com/infobloxopen/atlas-app-toolkit/gorm/resource"
+)
+
+func main() {
+	doneC := make(chan error)
+	logger := NewLogger()
+	pubsub, err := dapr.InitPubsub(viper.GetString("dapr.subscribe.topic"), viper.GetString("dapr.pubsub.name"), viper.GetInt("dapr.appPort"), viper.GetInt("dapr.grpcport"), logger)
+	if err != nil {
+		logger.Fatalf("Cannot initialize pubsub: %v", err)
+	}
+	if viper.GetBool("internal.enable") {
+		go func() { doneC <- ServeInternal(logger) }()
+	}
+
+	go func() { doneC <- ServeExternal(logger, pubsub) }()
+
+	if err := <-doneC; err != nil {
+		logger.Fatal(err)
+	}
+}
+
+func NewLogger() *logrus.Logger {
+	logger := logrus.StandardLogger()
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logger.SetReportCaller(true)
+
+	// Set the log level on the default logger based on command line flag
+	if level, err := logrus.ParseLevel(viper.GetString("logging.level")); err != nil {
+		logger.Errorf("Invalid %q provided for log level", viper.GetString("logging.level"))
+		logger.SetLevel(logrus.InfoLevel)
+	} else {
+		logger.SetLevel(level)
+	}
+
+	return logger
+}
+
+// ServeInternal builds and runs the server that listens on InternalAddress
+func ServeInternal(logger *logrus.Logger) error {
+
+	s, err := server.NewServer(
+		// register metrics
+		server.WithHandler("/metrics", promhttp.Handler()),
+	)
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:%s", viper.GetString("internal.address"), viper.GetString("internal.port")))
+	if err != nil {
+		return err
+	}
+
+	logger.Debugf("serving internal http at %q", fmt.Sprintf("%s:%s", viper.GetString("internal.address"), viper.GetString("internal.port")))
+	return s.Serve(nil, l)
+}
+
+// ServeExternal builds and runs the server that listens on ServerAddress and GatewayAddress
+func ServeExternal(logger *logrus.Logger, pubsub *dapr.PubSub) error {
+
+	if viper.GetString("database.dsn") == "" {
+		setDBConnection()
+	}
+	grpcServer, err := NewGRPCServer(logger, pubsub, viper.GetString("database.dsn"))
+	if err != nil {
+		logger.Fatalln(err)
+	}
+	grpc_prometheus.Register(grpcServer)
+
+	s, err := server.NewServer(
+		server.WithGrpcServer(grpcServer),
+	)
+	if err != nil {
+		logger.Fatalln(err)
+	}
+
+	grpcL, err := net.Listen("tcp", fmt.Sprintf("%s:%s", viper.GetString("server.address"), viper.GetString("server.port")))
+	if err != nil {
+		logger.Fatalln(err)
+	}
+
+	logger.Printf("serving gRPC at %s:%s", viper.GetString("server.address"), viper.GetString("server.port"))
+
+	return s.Serve(grpcL, nil)
+}
+
+func init() {
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AddConfigPath(viper.GetString("config.source"))
+	if viper.GetString("config.file") != "" {
+		log.Printf("Serving from configuration file: %s", viper.GetString("config.file"))
+		viper.SetConfigName(viper.GetString("config.file"))
+		if err := viper.ReadInConfig(); err != nil {
+			log.Fatalf("cannot load configuration: %v", err)
+		}
+	} else {
+		log.Printf("Serving from default values, environment variables, and/or flags")
+	}
+	resource.RegisterApplication(viper.GetString("app.id"))
+	resource.SetPlural()
+}
+
+// setDBConnection sets the db connection string
+func setDBConnection() {
+	viper.Set("database.dsn", fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=%s dbname=%s",
+		viper.GetString("database.address"), viper.GetString("database.port"),
+		viper.GetString("database.user"), viper.GetString("database.password"),
+		viper.GetString("database.ssl"), viper.GetString("database.name")))
+}
